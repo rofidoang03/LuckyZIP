c602463b77584a95aac0abb9694e696c603ce038
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-01-01 19:04:55 +0700

    Update lucky-zip.py

--- lucky-zip.py
+++ lucky-zip.py
@@ -2,6 +2,12 @@ import zipfile
 import sys
 from tqdm import tqdm
 import time
+import pyfiglet
+
+def display_banner():
+    banner = pyfiglet.figlet_format("LuckyZip", font="standard")
+    colored_banner = f"\033[1;96m{banner}\033[0m"  # Blue color ANSI escape codes
+    print(colored_banner)
 
 def crack_zip(zip_file, wordlist, start_time):
     try:
@@ -42,10 +48,11 @@ def crack_zip(zip_file, wordlist, start_time):
     print(f"Duration: {duration:.2f} seconds")
 
 def main():
-    print("LucyZip - ZIP Password Cracker")
+    display_banner()
+    print("ZIP Password Cracker")
     print("This tool can be found at: <https://github.com/rofidoang03/LuckyZip>\n")
     if len(sys.argv) != 5 or sys.argv[1] != '-f' or sys.argv[3] != '-w':
-        print("\nUsage: python script.py -f [zip_file] -w [wordlist]")
+        print("Usage: python script.py -f [zip_file] -w [wordlist]")
         return
     
     zip_file = sys.argv[2]
