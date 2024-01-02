512065990db1c4a09230dbab9217c4d412d47230
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-01-02 18:16:27 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -3,6 +3,17 @@
 
 scripts=("lucky-zip-cli" "lucky-zip-gui")
 chmod +x "${scripts[@]}"
-path="/usr/bin/"
-mv "${scripts[@]}" "${path}"
-echo "Proses instalasi selesai."
+cek=$(uname -o)
+if [[ "${cek}" == "Android" ]]; then
+    path="/data/data/com.termux/files/usr/bin"
+    mv "${scripts[@]}" "${path}"
+    pip3 install -r android.txt
+    echo "Proses instalasi selesai."
+elif [[ "${cek}" == "GNU/Linux" ]]; then
+    path="/usr/bin"
+    mv "${scripts[@]}" "${path}" 
+    pip3 install linux.txt
+    echo "Proses instalasi selesai."
+fi
+    
+   
